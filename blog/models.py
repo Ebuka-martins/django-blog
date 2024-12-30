54335d498b16171239d1f7c24a4a865cf7060a1d
Ebuka MartinsEchesi <martin4best@gmail.com>
2024-12-30 17:39:43 +0000

    update comment

--- blog/models.py
+++ blog/models.py
@@ -16,6 +16,11 @@ created_on = models.DateTimeField(auto_now_add=True)
 status = models.IntegerField(choices=STATUS, default=0)
 excerpt = models.TextField(blank=True)
 updated_on = models.DateTimeField(auto_now=True)
+class Meta:
+        ordering = ["-created_on"]
+
+def __str__(self):
+        return f"{self.title} | written by {self.author}"       
 
 # create comment models
 class Comment(models.Model):
@@ -23,9 +28,18 @@ class Comment(models.Model):
         Post, on_delete=models.CASCADE, related_name="comments")
     author = models.ForeignKey(
         User, on_delete=models.CASCADE, related_name="commenter")
-    body = models.TextField()
+    content = models.TextField()  # Changed from body to content
     approved = models.BooleanField(default=False)
     created_on = models.DateTimeField(auto_now_add=True)
+    
+    class Meta:
+        ordering = ["created_on"]
+
+    def __str__(self):
+        return f"Comment {self.content} by {self.author}"  # Changed here too 
+
+  
+
 
 # Event model
 class Event(models.Model):
