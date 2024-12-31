553ee1b0c86fd6590b6ef68b0646d0977cf0c646
Ebuka MartinsEchesi <martin4best@gmail.com>
2024-12-31 13:01:50 +0000

    add and importing url

--- codestar/urls.py
+++ codestar/urls.py
@@ -16,10 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-  
 
 urlpatterns = [
+    path("", include("blog.urls"), name="blog-urls"),
     path('admin/', admin.site.urls),
-    path("", include("blog.urls"), name="blog-urls"),  # Add this line to include the blog path
 ]
 
